b858c23f8102fc41f9b723cc7a1d547b2e36e68f
Maja Kabus <kabusm@google.com>
2018-09-10 03:04:03 -0700

    Tests from https://github.com/WICG/trusted-types/tree/master/tests ported to WPT
    Additional tests and checks adapted from polyfill test added.
    Trusted Types tests restructured to make test files smaller.
    
    [Some tests still fail]
    
    Bug: 739170
    Change-Id: I56563c16facd7c17d79e7b5625115a0f045618f8

--- trusted-types/support/helper.sub.js
+++ trusted-types/support/helper.sub.js
@@ -75,13 +75,17 @@ function assert_element_accepts_trusted_type(tag, attribute, value, expected) {
   let elem = document.createElement(tag);
   elem[attribute] = value;
   assert_equals(elem[attribute] + "", expected);
+  assert_equals(elem.getAttribute(attribute), expected);
 }
 
 function assert_throws_no_trusted_type(tag, attribute, value) {
   let elem = document.createElement(tag);
+  let prev = elem[attribute];
   assert_throws(new TypeError(), _ => {
     elem[attribute] = value;
   });
+  assert_equals(elem[attribute], prev);
+  assert_equals(elem.getAttribute(attribute), prev);
 }
 
 function assert_element_accepts_trusted_html_explicit_set(win, c, t, tag, attribute, expected) {
@@ -112,17 +116,22 @@ function assert_element_accepts_trusted_type_explicit_set(tag, attribute, value,
   let elem = document.createElement(tag);
   elem.setAttribute(attribute, value);
   assert_equals(elem[attribute] + "", expected);
+  assert_equals(elem.getAttribute(attribute), expected);
 }
 
 function assert_throws_no_trusted_type_explicit_set(tag, attribute, value) {
   let elem = document.createElement(tag);
+  let prev = elem[attribute];
   assert_throws(new TypeError(), _ => {
     elem.setAttribute(attribute, value);
   });
+  assert_equals(elem[attribute], prev);
+  assert_equals(elem.getAttribute(attribute), prev);
 }
 
 function assert_element_accepts_non_trusted_type_explicit_set(tag, attribute, value, expected) {
   let elem = document.createElement(tag);
   elem.setAttribute(attribute, value);
   assert_equals(elem[attribute] + "", expected);
+  assert_equals(elem.getAttribute(attribute), expected);
 }
